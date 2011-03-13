7dbd51752c3028a8f7bd9b53ceb8cdf5fc6a7b5d
Ben Foxall <benfoxall@gmail.com>
2011-03-13 17:33:05 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codescribe::Application.config.secret_token = 'e65964b517130e2b6e4f3ff9e3f394701ccc6262fc117bc507e3d568e21185d0a604d072f320a7cbe234137bec1aacac99db90f5367baf5397915aaa903a7f36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Codescribe::Application.config.session_store :cookie_store, :key => '_codescribe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Codescribe::Application.config.session_store :active_record_store
